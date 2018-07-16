a55a4eb9c02d49c5c19e53a6929136d7abd468d2
Daniel Parks <daniel.parks@bbc.co.uk>
2018-07-16 12:27:15 +0100

    = stop searching for words as soon as all of them are found

--- src/main/scala/com/delprks/searcher/analyse/Finder.scala
+++ src/main/scala/com/delprks/searcher/analyse/Finder.scala
@@ -1,14 +1,33 @@
 package com.delprks.searcher.analyse
 
+import scala.collection.mutable
+import util.control.Breaks._
+
 object Finder {
   final val UNSUPPORTED_LETTERS_PATTERN = "[^a-z' ]"
 
-  def find(words: Set[String], file: IndexedSeq[String]): Set[String] = {
-    val result: List[String] = file.toParArray.flatMap { line =>
-      stringToUniqueWords(line).filter(words)
-    }.toList
+  // to speed up the search, this method will stop looking or keywords (break out of the loop) as soon as all of them have been found
+  def find(words: Set[String], lines: IndexedSeq[String]): Set[String] = {
+    val resultContainer = new mutable.HashMap[String, Boolean]()
 
-    result.toSet
+    words.map(word => resultContainer += (word -> false))
+
+    breakable {
+      lines.toParArray.foreach { line =>
+        // only search for words that haven't been found yet
+        val wordsToSearchFor = resultContainer.filter(_._2 == false).keys.toSet
+
+        if (wordsToSearchFor.isEmpty) {
+          break
+        }
+
+        val foundWords = stringToUniqueWords(line).filter(wordsToSearchFor)
+
+        foundWords.foreach(word => resultContainer(word) = true)
+      }
+    }
+
+    resultContainer.filter(_._2 == true).keys.toSet
   }
 
   def stringToUniqueWords(input: String): Set[String] = input.toLowerCase.replaceAll(UNSUPPORTED_LETTERS_PATTERN, "").split(" ").toSet
